b0123d73b02e7f77bcf4de61c65b71f57c530b2a
cybyacine <yacine.benouirane@esprit.tn>
2019-06-10 05:32:57 +0100

    first commit

---
+++ chercherTousEvenements.php
@@ -0,0 +1,18 @@
+<?php header('Access-Control-Allow-Origin: *'); ?>
+<?php 
+require_once('connexion.php'); 
+      $query_search = "SELECT * FROM `evenement`";  
+      $jsontext = "[";
+      $stmt1 = $pdo->query($query_search, PDO::FETCH_OBJ);
+			foreach($stmt1->fetchAll() as $row) {
+				$jsontext .="{";
+				foreach($row as $key => $value) {
+	    			$jsontext .= '"'.addslashes($key).'": "'.addslashes($value).'",';
+					}
+					$jsontext = substr_replace($jsontext, '', -1);
+					$jsontext .="},";
+	}
+	$jsontext = substr_replace($jsontext, '', -1);
+	$jsontext .= "]";
+	echo $jsontext;
+?>
